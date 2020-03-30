3deead573be300dac7c4cd46a6b95e2c3124f3df
lilbonito <sauteboy@gmail.com>
2020-03-29 18:06:47 -0700

    added a 7 sided mesh

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var THREE = require('three');
+let THREE = require('three');
 
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
+let renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = THREE.Mesh(geometry, material);
+let geometry = new THREE.BoxGeometry();
+let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+let cube = THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
@@ -21,3 +21,24 @@ const animate = () => {
 };
 
 animate();
+
+const verticesOfCube = [
+  -1, -1, -1,    1, -1, -1,    1,  1, -1,    -1,  1, -1,
+  -1, -1,  1,    1, -1,  1,    1,  1,  1,    -1,  1,  1,
+];
+
+const indicesOfFaces = [
+  2, 1, 0,    0, 3, 2,
+  0, 4, 7,    7, 3, 0,
+  0, 1, 5,    5, 4, 0,
+  1, 2, 6,    6, 5, 1,
+  2, 3, 7,    7, 6, 2,
+  4, 5, 6,    6, 7, 4,
+];
+
+let polyGeometry = new THREE.PolyhedronBufferGeometry( verticesOfCube, indicesOfFaces, radius, detail);
+let radius = 7;
+let detail= 2;
+let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+let sevenSides = THREE.Mesh(polyGeometry, material);
+scene.add(sevenSides);
